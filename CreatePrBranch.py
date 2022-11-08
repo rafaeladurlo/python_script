54abca7c0d7c5d73c8059ba10a4f4e72acbeacad
rafaaprodrigues <55592520+rafaaprodrigues@users.noreply.github.com>
2022-11-08 19:07:31 -0300

    Update CreatePrBranch.py

--- CreatePrBranch.py
+++ CreatePrBranch.py
@@ -11,7 +11,7 @@ print("                                                        ")
 
 def CheckmarxLogin():
     urlToken = cxServer + "/cxrestapi/auth/identity/connect/token"
-    scope = "access_control_api%20sast_api"
+    scope = "sast_api"
     clientId = "resource_owner_sast_client"
     payload = 'username=' + cxUser +'&password=' + cxPwd + '&grant_type=password&scope=' + scope + '&client_id=' + clientId +'&client_secret=014DF517-39D1-4453-B7B3-9930C563627C'
     headersToken = {'Content-Type': 'application/x-www-form-urlencoded'}
