4be21e401be92cde0d8f1989662dd8c72768677d
rafaaprodrigues <55592520+rafaaprodrigues@users.noreply.github.com>
2022-12-21 16:35:19 -0300

    Update commentScript.groovy

--- commentScript.groovy
+++ commentScript.groovy
@@ -5,12 +5,5 @@ import groovy.json.JsonSlurper
 println("------------- Groovy script execution started --------------------")
 println("Checking sast comment")
 
-static void main(String[] args) {
-    comments(args[1], args[2]);
-} 
-
-def comments(fbuildnum, fprnum) { 
-   String SAST_Comment = "Build: " + fbuildnum + " PR Id: " + fprnum;
-   println(SAST_Comment)
-   return SAST_Comment;
-}
+String SAST_Comment = "TESTE";
+return SAST_Comment;
