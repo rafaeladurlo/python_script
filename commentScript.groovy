0c5189fafb5f871cce935244db007ae1e89cc2fb
rafaaprodrigues <55592520+rafaaprodrigues@users.noreply.github.com>
2022-12-22 11:32:23 -0300

    Update commentScript.groovy

--- commentScript.groovy
+++ commentScript.groovy
@@ -1,12 +1,9 @@
 @Grab(group='org.codehaus.groovy.modules.http-builder', module='http-builder', version='0.7' )
+
 import groovyx.net.http.HTTPBuilder
 
-def repoUrl = request.getRepoUrl()
-def branch = request.getBranch()
-def commitId = request.getHash()
+def myVar = build.getEnvironment(listener).get('${CIRCLE_BRANCH}')
 
-String scanComment = "ScanRequest(namespace=" + request.getNamespace() + ", application=" + request.getApplication() + ", org=" + request.getOrg() + ", team=" + request.getTeam() + ", project=" + request.getProject() + ", cxFields=" + request.getCxFields() + ", site=" + request.getSite() + ", repoUrl=" + request.getRepoUrl() + ", repoName=" + request.getRepoName() + ", branch=" + request.getBranch() + ", mergeTargetBranch=" + request.getMergeTargetBranch() + ", mergeNoteUri=" + request.getMergeNoteUri() + ", repoProjectId=" + request.getRepoProjectId() + ", refs=" + request.getRefs() + ", email=" + request.getEmail() + ", incremental=" + request.isIncremental() + ", scanPreset=" + request.getScanPreset() + ", excludeFiles=" + request.getExcludeFiles() + ", excludeFolders=" + request.getExcludeFolders() + ", repoType=" + request.getRepoType() + ", product=" + request.getProduct() + ", bugTracker=" + request.getBugTracker() + ", type=" + request.getType() + ", activeBranches=" + request.getActiveBranches() + ", filter=" + request.getFilter()+ ", scanResubmit=" + request.getScanResubmit() + ")";
-
-println "INFO : Scanning code from $scanComment"
+String scanComment = "Repo: $myVar"
 
 return scanComment
