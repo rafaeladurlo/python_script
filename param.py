e7c5f8542f9931e556481121f7a2c93eef0116d3
rafaaprodrigues <55592520+rafaaprodrigues@users.noreply.github.com>
2022-12-22 14:35:18 -0300

    Update param.py

--- param.py
+++ param.py
@@ -7,7 +7,7 @@ print("Criando arquivo com parametros:")
 build_id = sys.argv[1]
 pr_id = sys.argv[2]
 
-commentario = "Build Id: " + build_id + "PR Id: " + pr_id
+commentario = "Build Id: " + build_id + " PR Id: " + pr_id
 
 f = open("param.txt", "a")
 f.write(commentario)
