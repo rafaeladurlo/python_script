fa2e90be23ff7fd2973f00bd6b940d0ceded0600
rafaaprodrigues <55592520+rafaaprodrigues@users.noreply.github.com>
2022-12-22 10:58:09 -0300

    Update commentScript.groovy

--- commentScript.groovy
+++ commentScript.groovy
@@ -4,11 +4,8 @@ import groovyx.net.http.HTTPBuilder
 def repoUrl = request.getRepoUrl()
 def branch = request.getBranch()
 def commitId = request.getHash()
-//When using SCM automated workflows, comment the line above and uncomment the 2 lines below so the hash is not null (example is for Github Actions):
-//f env = System.getenv()
-//f commitId = env['GITHUB_SHA']
 
-String scanComment = "Repo: $repoUrl | Branch: $branch | Commit ID: $commitId"
+String scanComment = "ScanRequest(namespace=" + request.getNamespace() + ", application=" + request.getApplication() + ", org=" + request.getOrg() + ", team=" + request.getTeam() + ", project=" + request.getProject() + ", cxFields=" + request.getCxFields() + ", site=" + request.getSite() + ", repoUrl=" + request.getRepoUrl() + ", repoName=" + request.getRepoName() + ", branch=" + request.getBranch() + ", mergeTargetBranch=" + request.getMergeTargetBranch() + ", mergeNoteUri=" + request.getMergeNoteUri() + ", repoProjectId=" + request.getRepoProjectId() + ", refs=" + request.getRefs() + ", email=" + request.getEmail() + ", incremental=" + request.isIncremental() + ", scanPreset=" + request.getScanPreset() + ", excludeFiles=" + request.getExcludeFiles() + ", excludeFolders=" + request.getExcludeFolders() + ", repoType=" + request.getRepoType() + ", product=" + request.getProduct() + ", bugTracker=" + request.getBugTracker() + ", type=" + request.getType() + ", activeBranches=" + request.getActiveBranches() + ", filter=" + request.getFilter()+ ", scanResubmit=" + request.getScanResubmit() + ")";
 
 println "INFO : Scanning code from $scanComment"
 
